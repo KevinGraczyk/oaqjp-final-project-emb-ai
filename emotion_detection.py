a4832fe0f389e80c873c2323960983e14843f396
KevinGraczyk <kevingraczyk22512@gmail.com>
2026-01-05 09:38:28 -0500

    Initial Commit

---
+++ emotion_detection.py
@@ -0,0 +1,26 @@
+"""
+Module for detecting emotions in text using Watson NLP API. 
+"""
+import requests
+
+def emotion_detector(text_to_analyze):
+    """
+    Anaalyze the emotions of a given text using API
+    """
+    url = (
+        'https://sn-watson-emotion.labs.skills.network/'
+        'v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    )
+    headers = {
+        "grpc-metadata-mm-model-id": 
+        "emotion_aggregated-workflow_lang_en_stock"
+    }
+    myobj = { 
+        "raw_document": { 
+            "text": text_to_analyze 
+        } 
+    }
+
+    response = requests.post(url, json=myobj, headers=headers, timeout=10)
+
+    return response.text
